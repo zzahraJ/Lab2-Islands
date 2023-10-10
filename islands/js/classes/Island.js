a3117fee8591cee034ab23e0a2c04a5fe6e443ba
zzahraJ <r0883814@student.thomasmore.be>
2023-10-10 14:16:56 +0200

    feat: add the render function

--- islands/js/classes/Island.js
+++ islands/js/classes/Island.js
@@ -1,14 +1,23 @@
 export default class Island {
-  constructor(name) {}
+  constructor(name) {
+    this.name = name || this.getRandomName();
+    this.coordinates = this.getCoords();
+    this.color = this.getRandomColor();
+  }
 
   getRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-  remove() {
-    // JS animations api, fade out
-    // remove the element when the animation ended
+  getCoords() {
+    // return coordinates within the screen at random, feel free to change it up!
+    let randomSign = Math.random() < 0.5 ? -1 : 1;
+    return {
+      x: ((Math.random() * window.innerWidth) / 2) * randomSign,
+      y: ((Math.random() * window.innerHeight) / 2) * randomSign
+    };
   }
+
 
   getRandomName() {
     // array with 10 random island names
@@ -26,5 +35,42 @@ export default class Island {
     ];
 
     // return a random name from the array
+    return names[Math.floor(Math.random() * names.length)];
+  }
+
+  render(){
+    // create a new element
+    let div = document.createElement("div");
+    
+    // add the island class to the element
+    div.classList.add("island");
+    
+    // set the background color of the element
+    div.style.backgroundColor = this.getRandomColor();
+
+    let element = document.createElement("div");
+    element.classList.add("island-name");
+    element.innerHTML = this.name;
+    div.appendChild(element);
+
+    // set the coordinates of the element
+
+    document.body.appendChild(div);
+    let coords = this.getCoordinates();
+    div.animate([
+      { transform: `translate(0px, 0px)` },
+      { transform: `translate(${coords.x}px, ${coords.y}px)` }
+    ], {
+      duration: 1000,
+      iterations: 1,
+      fill: "forwards"
+    });
+  }
+  JSON() {
+    return {
+      name: this.name,
+      coordinates: this.coordinates,
+      color: this.color
+    };
   }
 }
