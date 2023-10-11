494598cbb7850671929abef180b116cef3027f89
zzahraJ <r0883814@student.thomasmore.be>
2023-10-11 15:15:19 +0200

    feat: delete the constructor

--- islands/js/classes/Island.js
+++ islands/js/classes/Island.js
@@ -1,16 +1,10 @@
 export default class Island {
-  constructor(name) {
-    this.name = name || this.getRandomName();
-    this.coordinates = this.getCoords();
-    this.color = this.getRandomColor();
-  }
-
   getRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
   getCoords() {
-    // return coordinates within the screen at random, feel free to change it up!
+    // return coords within the screen at random, feel free to change it up!
     let randomSign = Math.random() < 0.5 ? -1 : 1;
     return {
       x: ((Math.random() * window.innerWidth) / 2) * randomSign,
@@ -38,39 +32,45 @@ export default class Island {
     return names[Math.floor(Math.random() * names.length)];
   }
 
-  render(){
-    // create a new element
-    let div = document.createElement("div");
+  // constructor(name) {
+  //   this.name = name || this.getRandomName();
+  //   this.coordinates = this.getCoords();
+  //   this.color = this.getRandomColor();
+  // }
+
+  // render(){
+  //   // create a new element
+  //   let div = document.createElement("div");
     
-    // add the island class to the element
-    div.classList.add("island");
+  //   // add the island class to the element
+  //   div.classList.add("island");
     
-    // set the background color of the element
-    div.style.backgroundColor = this.getRandomColor();
+  //   // set the background color of the element
+  //   div.style.backgroundColor = this.getRandomColor();
 
-    let element = document.createElement("div");
-    element.classList.add("island-name");
-    element.innerHTML = this.name;
-    div.appendChild(element);
+  //   let element = document.createElement("div");
+  //   element.classList.add("island-name");
+  //   element.innerHTML = this.name;
+  //   div.appendChild(element);
 
-    // set the coordinates of the element
+  //   // set the coordinates of the element
 
-    document.body.appendChild(div);
-    let coords = this.getCoordinates();
-    div.animate([
-      { transform: `translate(0px, 0px)` },
-      { transform: `translate(${coords.x}px, ${coords.y}px)` }
-    ], {
-      duration: 1000,
-      iterations: 1,
-      fill: "forwards"
-    });
-  }
-  JSON() {
-    return {
-      name: this.name,
-      coordinates: this.coordinates,
-      color: this.color
-    };
-  }
+  //   document.body.appendChild(div);
+  //   let coords = this.getCoordinates();
+  //   div.animate([
+  //     { transform: `translate(0px, 0px)` },
+  //     { transform: `translate(${coords.x}px, ${coords.y}px)` }
+  //   ], {
+  //     duration: 1000,
+  //     iterations: 1,
+  //     fill: "forwards"
+  //   });
+  // }
+  // JSON() {
+  //   return {
+  //     name: this.name,
+  //     coordinates: this.coordinates,
+  //     color: this.color
+  //   };
+  // }
 }
